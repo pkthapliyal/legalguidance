079f51d5b4a8cf924e5ecca06bbfa07b6d0cd47a
Pankaj Kumar <121335947+pkthapliyal@users.noreply.github.com>
2024-02-05 02:17:00 +0530

    Update appointment.route.js

--- Legal-Backend/routes/appointment.route.js
+++ Legal-Backend/routes/appointment.route.js
@@ -173,9 +173,6 @@ appointRoute.get("/:lawyerId/:clientId", async (req, res) => {
 
 //  Nodemails ---------------->>>>>>>>>>>>>;
 async function main(subject, details, Time, date, name, from, to) {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
